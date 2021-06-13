6505d8fa53d14f20bf11dee17bdc29c7ab9ffdc3
Paulo <pauloconles@gmail.com>
2021-06-13 18:23:28 +0200

    Etiquetas Dao

--- Model/DAOs/EtiquetasDao/EtiquetasDaoEntityFramework.cs
+++ Model/DAOs/EtiquetasDao/EtiquetasDaoEntityFramework.cs
@@ -21,10 +21,117 @@ namespace Es.Udc.DotNet.Photogram.Model.DAOs
         /// </summary>
         public EtiquetasDaoEntityFramework()
         {
+            
         }
 
         #endregion Public Constructors
 
+        /// <exception cref="InstanceNotFoundException"></exception>
+        public Publicaciones[] GetPublicaciones(string tag, int npag)
+        {
+            DbSet<EtiquetaSet> etiq = Context.Set<EtiquetaSet>();
 
+            try
+            {
+                var request =
+                    (from e in etiq.Include("Publicaciones")
+                     where e.tag == tag
+                     select e);
+                EtiquetaSet etiqueta = request.FirstOrDefault();
+                return etiqueta.Publicaciones.Skip(10 * npag).Take(10).ToArray<Publicaciones>();
+            }
+            catch (ArgumentNullException)
+            {
+                throw new InstanceNotFoundException(tag, "EtiquetaSet");
+            }
+        }
+
+        /// <exception cref="InstanceNotFoundException"></exception>
+        public EtiquetaSet[] GetEtiquetas(long pubId)
+        {
+            DbSet<Publicaciones> pubs = Context.Set<Publicaciones>();
+
+            try
+            {
+                var request =
+                    (from p in pubs.Include("EtiquetaSet")
+                     where p.Id == pubId
+                     select p);
+                Publicaciones publi = request.FirstOrDefault();
+                return publi.EtiquetaSet.ToArray<EtiquetaSet>();
+            }
+            catch (ArgumentNullException)
+            {
+                throw new InstanceNotFoundException(pubId, "Publicaciones");
+            }
+        }
+
+        /// <exception cref="InstanceNotFoundException"></exception>
+        public void Etiquetar(string tag, long pubId)
+        {
+            DbSet<EtiquetaSet> etiquetas = Context.Set<EtiquetaSet>();
+            DbSet<Publicaciones> publicaciones = Context.Set<Publicaciones>();
+
+            try
+            {
+                var result = (from u in etiquetas.Include("Publicaciones")
+                              where u.tag == tag
+                              select u);
+                EtiquetaSet et = result.FirstOrDefault();
+
+                try
+                {
+                    var result2 = (from p in publicaciones
+                                   where p.Id == pubId
+                                   select p);
+                    Publicaciones pub = result2.FirstOrDefault();
+
+                    et.Publicaciones.Add(pub);
+                    Update(et);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new InstanceNotFoundException(pubId, "Publicaciones");
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                throw new InstanceNotFoundException(tag, "EtiquetaSet");
+            }
+        }
+
+        /// <exception cref="InstanceNotFoundException"></exception>
+        public void Desetiquetar(string tag, long pubId)
+        {
+            DbSet<EtiquetaSet> etiquetas = Context.Set<EtiquetaSet>();
+            DbSet<Publicaciones> publicaciones = Context.Set<Publicaciones>();
+
+            try
+            {
+                var result = (from u in etiquetas.Include("Publicaciones")
+                              where u.tag == tag
+                              select u);
+                EtiquetaSet et = result.FirstOrDefault();
+
+                try
+                {
+                    var result2 = (from p in publicaciones
+                                   where p.Id == pubId
+                                   select p);
+                    Publicaciones pub = result2.FirstOrDefault();
+
+                    et.Publicaciones.Remove(pub);
+                    Update(et);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new InstanceNotFoundException(pubId, "Publicaciones");
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                throw new InstanceNotFoundException(tag, "EtiquetaSet");
+            }
+        }
     }
 }
--- Model/DAOs/EtiquetasDao/IEtiquetasDao.cs
+++ Model/DAOs/EtiquetasDao/IEtiquetasDao.cs
@@ -6,6 +6,12 @@ namespace Es.Udc.DotNet.Photogram.Model.DAOs
 {
     public interface IEtiquetasDao : IGenericDao<EtiquetaSet, string>
     {
+        Publicaciones[] GetPublicaciones(string tag, int npag);
 
+        EtiquetaSet[] GetEtiquetas(long pubId);
+
+        void Etiquetar(string tag, long pubId);
+
+        void Desetiquetar(string tag, long pubId);
     }
 }
